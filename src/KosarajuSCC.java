4164114e28ccbea807a9c9ec2e452af8e92ddf9f
vpathalam <vpathalam@gmail.com>
2020-09-17 22:37:09 -0400

    updated desc

--- src/KosarajuSCC.java
+++ src/KosarajuSCC.java
@@ -7,6 +7,18 @@ import java.util.*;
 //Purpose: Linear time algorithm to find the strongly connected components of a directed graph
 //Algorithm can break down a graph into its Strongly Connected Components, ordered by descending finish time
 //Outputs the size of the sink SCC (the one that finishes first), else 0
+
+/*
+GeeksForGeeks Algorithm Description:
+We can find all strongly connected components in O(V+E) time using Kosaraju’s algorithm. Following is detailed Kosaraju’s algorithm.
+1) Create an empty stack ‘S’ and do DFS traversal of a graph.
+In DFS traversal, after calling recursive DFS for adjacent vertices of a vertex, push the vertex to stack.
+2) Reverse directions of all arcs to obtain the transpose graph.
+3) One by one pop a vertex from S while S is not empty.
+Let the popped vertex be ‘v’. Take v as source and do DFS (call DFSUtil(v)).
+The DFS starting from v prints strongly connected component of v.
+In the above example, we process vertices in order 0, 3, 4, 2, 1 (One by one popped from stack).
+ */
 public class KosarajuSCC {
 
   int nodeCount;
